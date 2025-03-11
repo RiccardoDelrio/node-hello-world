56bcd36b666bb70d77b621d2d55a8df5f9d9fe65
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-11 13:24:20 +0100

    update index.js to log command line arguments and extract the first message

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+console.log("ciao", process.argv[2]);
+
+const argv = process.argv.slice(2) //ho preso solo la posizione 2  (la pos 0 è node, la pos 1 è il percorso, e la due è il messaggio che ci abbino dopo)
+console.log(argv)//esce sottoforma di array
+const messaggio = argv[0] //in questo caso contiene solo un dato ed è in posizione 0
+console.log("Estrazione del messaggio in posizione 0 da un array", messaggio)
